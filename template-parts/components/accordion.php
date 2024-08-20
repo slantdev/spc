d8d17cb5389e0f270c844662aa77b7c9c887809c
Slant Dev <build@amazeworx.com>
2024-08-20 10:16:19 +0700

    accordion

--- template-parts/components/accordion.php
+++ template-parts/components/accordion.php
@@ -24,7 +24,7 @@ if ($accordion_repeater) { ?>
     ?>
       <div class="collapse collapse-plus bg-brand-light-gray rounded-md mb-6">
         <input type="checkbox" class="accordion-btn w-full h-full block" name="<?php echo $accordion_id ?>" />
-        <div class="collapse-title bg-[#E2E2E2] text-brand-light-blue text-lg lg:text-2xl text-left border-t-0 border-x-0 font-bold py-3 pl-4 pr-8 lg:py-5 lg:pl-8 lg:pr-12 after:font-thin after:!end-8 after:text-brand-light-blue after:!top-2 after:text-3xl after:lg:text-5xl">
+        <div class="collapse-title bg-[#E2E2E2] text-brand-light-blue text-lg lg:text-2xl text-left border-t-0 border-x-0 font-medium py-3 pl-4 pr-8 lg:py-5 lg:pl-8 lg:pr-12 after:font-thin after:!end-8 after:text-brand-light-blue after:!top-2 after:text-3xl after:lg:text-5xl">
           <?php echo $title ?>
         </div>
         <div class="collapse-content p-0">
